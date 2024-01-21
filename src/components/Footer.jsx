615bb67f01dfc91c169a15566263d0514f2da68f
jalvarez <esi.segurity@gmail.com>
2024-01-21 00:17:59 -0500

    creo footer
    se creo el componente footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import {Box,Stack,Typography} from "@mui/material"
+import Logo from "../assets/images/Logo-1.png"
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <Box mt="80x"  bgcolor="#fff3f4">
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+        <img src={Logo} alt="logo" width="200px" height="40px"/>
+        <Typography variant='h5' pb="40px" mt="20px">
+          Derechos Reservados ©️ 2024
+        </Typography>
+      </Stack>
+    </Box>
   )
 }
 
